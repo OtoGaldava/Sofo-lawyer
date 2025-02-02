112319f48f7aad28868775acabcae06ea0faca8b
OtoGaldava <oto.galdava22@gmail.com>
2025-02-02 20:40:50 +0400

    updated services

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -7,27 +7,27 @@ const Services = () => {
             <div className={styles.wrapper}>
 
                 <div className={styles.contentWrapper}>
-                    <div className={styles.subTitleWrapper}>
+                    {/* <div className={styles.subTitleWrapper}>
                         <div className={styles.line}></div>
-                        <h3 className={styles.subTitle}>პროფესიონალური საქმიანობა</h3>
+                        <h3 className={styles.subTitle}>მომსახურების სფერო</h3>
                         <div className={styles.line}></div>
-                    </div>
+                    </div> */}
 
-                    <h3 className={styles.title}>სერვისები</h3>
+                    <h3 className={styles.title}>მომსახურების სფერო</h3>
                 </div>
 
                 <div className={styles.servicesWrapper}>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'}></Service>
-                    <Service title={'სამოქალაქო საქმიანობა'} flex={true}></Service>
+                    <Service title={'მემკვიდრეობის სამართალი'}></Service>
+                    <Service title={'შრომის სამართალი'}></Service>
+                    <Service title={'საოჯახო სამართალი'}></Service>
+                    <Service title={'ვალდებულებითი სამართალი'}></Service>
+                    <Service title={'სახელშეკრულებლო სამართალი'}></Service>
+                    <Service title={'საბანკო სამართალი'}></Service>
+                    <Service title={'ადმინისტრაციული სამართალი'}></Service>
+                    <Service title={'მოქალაქეობის მიღება/მინიჭება'}></Service>
+                    <Service title={'უძრავი ქონების პრივატიზაცია'}></Service>
+                    <Service title={'ბინადრობის ნებართვები'}></Service>
+                    <Service title={'არასრულწლოვნებთან დაკავშირებული საკითხები'} flex={true}></Service>
                 </div>
             </div>
         </div>
