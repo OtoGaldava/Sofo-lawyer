00de10505a9c7521c71386292d1f1f60071c97eb
OtoGaldava <oto.galdava22@gmail.com>
2025-04-04 18:05:28 +0400

    update to load css and react

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  output: "export",
+  distDir: "dist",
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
+  basePath: "/Sofo-lawyer", 
+  assetPrefix: "/Sofo-lawyer/", 
 };
 
 export default nextConfig;
