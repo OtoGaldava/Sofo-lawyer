06d4b861c5ee3cfd2086bd811c18a1339eee8b80
OtoGaldava <oto.galdava22@gmail.com>
2025-04-22 18:23:41 +0400

    updated list

--- src/app/Components/bottom/bottom.tsx
+++ src/app/Components/bottom/bottom.tsx
@@ -23,11 +23,11 @@ const Bottom = () => {
                 <div className={styles.pages}>
                     <h3 className={styles.title}>გვერდები</h3>
 
-                    <ul className={styles.ul}>
+                    <div className={styles.ul}>
                         <Link href="/about" className={styles.li}>ჩვენ შესახებ</Link>
                         <Link href="/services" className={styles.li}>სერვისი</Link>
                         <Link href="/contacts" className={styles.li}>კონტაქტი</Link>
-                    </ul>
+                    </div>
                 </div>
 
                 <div className={styles.pages}>
@@ -41,7 +41,7 @@ const Bottom = () => {
 
                         <div className={styles.contactWrapper}>
                             <svg className={styles.phone} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill='#b68c5a' d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                            <div>ორშაბათი-შაბათი 10:00 - 18:00</div>
+                            <div>ორშაბათი-პარასკევი 10:00 - 18:00</div>
                         </div>
                     </div>
                 </div>
