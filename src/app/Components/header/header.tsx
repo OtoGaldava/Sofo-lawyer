d36244165730f53fc2d1ceebfb2341d0cd63a191
OtoGaldava <oto.galdava22@gmail.com>
2025-06-05 21:07:25 +0400

    changed links

--- src/app/Components/header/header.tsx
+++ src/app/Components/header/header.tsx
@@ -39,14 +39,14 @@ const Header = () => {
     }
 
     function handleClick () {
-        window.location.href = '/Sofo-lawyer'
+        window.location.href = '/'
     }
 
     return(
         <>
             <div className={ `${styles["navigation"]} ${styles['Pc']} ${scrolled? styles["sticky"] : ''}`}>
 
-                <img onClick={handleClick} className={styles.logo} src={'/Sofo-lawyer/logo-side.png'} alt='lawyer logo'></img>
+                <img onClick={handleClick} className={styles.logo} src={'/logo-side.png'} alt='lawyer logo'></img>
 
                 <div className={styles.wrapper}>
                     <div className={styles.listWrapper}>
@@ -63,7 +63,7 @@ const Header = () => {
 
             <div className={ `${styles["navigation"]} ${styles['Mobile']} ${scrolled? styles["sticky"] : ''}`}>
                 <div className={styles.mobWrapper}>
-                    <img onClick={handleClick} className={styles.logo} src={'/Sofo-lawyer/logo-side.png'} alt='logo of lawyer in tbilisi'></img>
+                    <img onClick={handleClick} className={styles.logo} src={'/logo-side.png'} alt='logo of lawyer in tbilisi'></img>
 
                     <div className={styles.button} onClick={changeButton}>
                         <BurgerMenu xMark={menu}></BurgerMenu>
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,8 +8,8 @@ const About = () => {
 
                 <div className={styles.knowledge}>
 
-                    <img className={styles.pcImage} alt='ადვოკატის ფოტო' src='/Sofo-lawyer/lawyer.webp'></img>            
-                    <img className={styles.mobImage} src="/Sofo-lawyer/lawyer.webp" alt="Best Image" />          
+                    <img className={styles.pcImage} alt='ადვოკატის ფოტო' src='/lawyer.webp'></img>            
+                    <img className={styles.mobImage} src="/lawyer.webp" alt="Best Image" />          
                     <div className={styles.textContainer}>
                         <h2 className={styles.headerText}>განათლება</h2>
                         <div className={styles.text}>
@@ -56,8 +56,8 @@ const About = () => {
                             <p className={styles.paragraph}>ხოლო 2025 წელს დააფუძნა სოფიო ბერძენიშვილის საადვოკატო ბიურო და ეწევა კერძო საადვოკატო საქმიანობას სამოქალაქო და ადმინისტრაციულ სამართალში,</p>                            
                         </div>
                     </div>
-                    <Image className={styles.pcImage} alt='პროფესიონალი ადვოკატი - სოფო ბერძენიშვილი და საადვოკატო ბიურო' width='400' height='650' src='/Sofo-lawyer/advokati.png'></Image> 
-                    <img className={styles.mobImage} src="/Sofo-lawyer/advokati.png" alt="პროფესიონალი advokati - სოფო ბერძენიშვილი და saadvokato biuro" />          
+                    <Image className={styles.pcImage} alt='პროფესიონალი ადვოკატი - სოფო ბერძენიშვილი და საადვოკატო ბიურო' width='400' height='650' src='/advokati.png'></Image> 
+                    <img className={styles.mobImage} src="/advokati.png" alt="პროფესიონალი advokati - სოფო ბერძენიშვილი და saadvokato biuro" />          
                 </div>
             </div>
         </div>
